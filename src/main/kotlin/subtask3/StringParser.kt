d0641eed9b3435abe2c921cc403c0a760edc2995
Anya Galyamova <galyamova.anya@gmail.com>
2021-05-25 20:59:02 +0300

    feat: implement tasks

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,36 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = arrayListOf<String>()
+        var finalSeparator: Char?
+        var deepCounter: Int
+
+        for ((index, letter) in inputString.withIndex()) {
+            if (separators.contains(letter)) {
+
+                deepCounter = 0
+                finalSeparator = separators[letter]
+
+                for (subIndex in index..inputString.lastIndex) {
+
+                    if (inputString[subIndex] == letter) { deepCounter++ }
+                    if (inputString[subIndex] == finalSeparator) {
+                        deepCounter--
+                        if (deepCounter == 0) {
+                            result.add(inputString.substring(index+1 until subIndex))
+                            break
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return result.toTypedArray()
+    }
+
+    companion object {
+        private val separators = mapOf<Char, Char>('<' to '>', '[' to ']', '(' to ')')
     }
 }
